a679b097518fbfb818027657a454aae1443b323a
nilihanth <nilihanth@users.noreply.github.com>
2016-01-24 12:23:07 -0600

    Use JQuery for DOM manipulation and Bootstrap CSS for index file formatting

--- BoM.js
+++ BoM.js
@@ -1,40 +1,51 @@
-// Script by Milo Housknecht. First developed ~ 8/15/15
-// Used http://www.w3schools.com for help.
-
+// Script by Milo Housknecht. Development started ~ 8/15/15
+// Used http://www.w3schools.com for help on loading XML file.
 
 // To be changed later; decides the current chapter.
-var currentChapter = 1;
+$(document).ready(function(){
+	var currentChapter = 1;
 
-function loadXMLDoc(filename) { 
-	// This function is the method in which an XML file can be loaded
-	xhttp=new XMLHttpRequest();
-	xhttp.open("GET",filename,false);
-	xhttp.send("");
-	return xhttp;
-}
-function nextChapter() {
-	currentChapter++;
-	showVerses(currentChapter);
-}
-function prevChapter() {
-	currentChapter--;
-}
-
-//Load the BoM file into 'x' and the XML DOM into 'xml'
-var x=loadXMLDoc("BoM.xml");
-var xml=x.responseXML;
-
-function showVerses (currentChapter) {
-	//This sets the access level.
-	var path="/root/book/chapter[" + currentChapter + "]/verse";
-	console.log("Path=" + path);
-	var nodes=xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
-	var result=nodes.iterateNext();
-
-	//Cycle through all verses in the 'currentChapter'
-	while (result) {
-		document.write(result.childNodes[0].nodeValue);
-		document.write("<br>");
-		result=nodes.iterateNext();
+	function loadXMLDoc(filename) { 
+		// This function is the method in which an XML file can be loaded
+		xhttp=new XMLHttpRequest();
+		xhttp.open("GET",filename,false);
+		xhttp.send("");
+		return xhttp;
 	}
-}
+	/*
+	function nextChapter() {
+		currentChapter++;
+		showChapter(currentChapter);
+	}
+	function prevChapter() {
+		currentChapter--;
+		showChapter(currentChapter);
+	}
+	*/
+	$('#btn-next-ch').click(function(){
+		showChapter(currentChapter++);
+	})
+	//Load the BoM file into 'x' and the XML DOM into 'xml'
+	var x=loadXMLDoc("BoM.xml");
+	var xml=x.responseXML;
+
+	function showChapter (currentChapter) {
+		//This sets the access level.
+		var path="/root/book/chapter[" + currentChapter + "]/verse";
+		console.log("Path=" + path);
+		var nodes=xml.evaluate(path, xml, null, XPathResult.ANY_TYPE, null);
+		var result=nodes.iterateNext();
+
+		$("li").remove();
+		//Cycle through all verses in the 'currentChapter'
+		while (result) {
+			var verse = result.childNodes[0].nodeValue;
+			$("ol").append("<li>" + verse + "</li>")
+			result=nodes.iterateNext();
+		}
+	}
+	showChapter(1);
+});
+
+
+
